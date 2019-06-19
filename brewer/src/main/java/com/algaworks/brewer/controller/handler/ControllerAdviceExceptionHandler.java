b2f056218feaa56fb2db243f3d7b81dc20fbc5a9
unknown <alvarorodrigues2010@gmail.com.br>
2019-06-19 15:54:48 -0300

    subindo a versão 12

---
+++ brewer/src/main/java/com/algaworks/brewer/controller/handler/ControllerAdviceExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.algaworks.brewer.controller.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
+
+@RestControllerAdvice
+public class ControllerAdviceExceptionHandler {
+	
+	@ExceptionHandler(NomeEstiloJaCadastradoException.class)
+	public ResponseEntity<String> handleNomeEstiloJaCadastradoException(NomeEstiloJaCadastradoException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+
+}
