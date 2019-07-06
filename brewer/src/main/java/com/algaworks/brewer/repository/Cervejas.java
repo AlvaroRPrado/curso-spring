d5a3889a59cc93acd95a2cb971275660a9c70bec
unknown <alvarorodrigues2010@gmail.com.br>
2019-07-06 12:01:54 -0300

    criação dos filter de pesquisas

--- brewer/src/main/java/com/algaworks/brewer/repository/Cervejas.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/Cervejas.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.filter.CervejaFilter;
 
 @Repository
 public interface Cervejas extends JpaRepository<Cerveja,  Long> {
 	
 	public Optional<Cerveja> findBySkuIgnoreCase(String sku);
+
+	public Object filtrar(CervejaFilter cervejaFilter);
 	
 }
