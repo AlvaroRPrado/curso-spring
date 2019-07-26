6d44f28da70d7080db5c7bef2048e27c421fc647
AlvaroRPrado <alvarorodrigues2010@gmail.com.br>
2019-07-26 17:45:41 -0300

    criação da bean paginação e correção do bug spaço finalizado a v015

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -6,23 +6,26 @@ import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.filter.CervejaFilter;
+import com.algaworks.brewer.repository.paginacao.PaginacaoUtil;
 
 public class CervejasImpl implements CervejasQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired
+	private PaginacaoUtil paginacaoUtil;
 	
 	
 	@SuppressWarnings("unchecked")
@@ -32,25 +35,10 @@ public class CervejasImpl implements CervejasQueries {
 		@SuppressWarnings("deprecation")
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);//fitros aba
 		
-		int paginaAtual = pageable.getPageNumber();
-		int totalRegistrosPorPagina = pageable.getPageSize();						//paginação
-		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
+		paginacaoUtil.preparar(criteria, pageable);
+		adicionarFiltro(filtro, criteria);
 		
-		criteria.setFirstResult(primeiroRegistro);												//paginação
-		criteria.setMaxResults(totalRegistrosPorPagina);
-		
-		//ordenação
-		Sort sort = pageable.getSort();
-		System.out.println(">>> sort: " + sort);
-		if (sort != null) {
-			Sort.Order order = sort.iterator().next();
-			String property = order.getProperty();
-			criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
-		}
-		
-adicionarFiltro(filtro, criteria);
-		
-		return new PageImpl<>(criteria.list(), pageable, total(filtro));//total de pagina
+		return new PageImpl<>(criteria.list(), pageable, total(filtro));
 	}
 		
 		private Long total(CervejaFilter filtro) {
