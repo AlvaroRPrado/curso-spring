6c105e3946145472fa4e9eed47eb25ab8c286018
AlvaroRPrado <alvarorodrigues2010@gmail.com.br>
2019-07-31 17:57:37 -0300

    Migração de tabela cliente , validação e agrupando validações para CPF ou CNPJ

--- brewer/src/main/java/com/algaworks/brewer/model/TipoPessoa.java
+++ brewer/src/main/java/com/algaworks/brewer/model/TipoPessoa.java
@@ -1,18 +1,23 @@
 package com.algaworks.brewer.model;
 
+import com.algaworks.brewer.model.validation.CpfGroup;
+import com.algaworks.brewer.model.validation.CnpjGroup;
+
 public enum TipoPessoa {
 
-	FISICA("Física", "CPF", "000.000.000-00"), 
-	JURIDICA("Jurídica", "CNPJ", "00.000.000/0000-00");
+	FISICA("Física", "CPF", "000.000.000-00", CpfGroup.class), 
+	JURIDICA("Jurídica", "CNPJ", "00.000.000/0000-00", CnpjGroup.class);
 
 	private String descricao;
 	private String documento;
 	private String mascara;
+	private Class<?>grupo;
 
-	TipoPessoa(String descricao, String documento, String mascara) {
+	TipoPessoa(String descricao, String documento, String mascara, Class<?> grupo) {
 		this.descricao = descricao;
 		this.documento = documento;
 		this.mascara = mascara;
+		this.grupo = grupo;
 	}
 
 	public String getDescricao() {
@@ -26,5 +31,11 @@ public enum TipoPessoa {
 	public String getMascara() {
 		return mascara;
 	}
+
+	public Class<?> getGrupo() {
+		return grupo;
+	}
+	
+	
 
 }
