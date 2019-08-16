eef5c32c85f316dd2c7773b8074e953b315aaaec
AlvaroRPrado <alvarorodrigues2010@gmail.com.br>
2019-08-15 23:29:39 -0300

    Carregando as permissões do usuário

--- brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -37,7 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 				.authorizeRequests()
 						.antMatchers("/cidades/nova").hasRole("CADASTRAR_CIDADE")
+						//.antMatchers("/cidades/nova").hasAnyAuthority("CADASTRAR_CIDADE")
 						.antMatchers("/usuarios/**").hasRole("CADASTRAR_USUARIO")
+					//	.antMatchers("/usuarios/**").hasAnyAuthority("CADASTRAR_USUARIO")
 						.anyRequest().authenticated()
 						.and()
 			.formLogin()
