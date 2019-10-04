7d4ae96cd8da5df011283927d2270d38307bdf1c
AlvaroRPrado <alvarorodrigues2010@gmail.com.br>
2019-10-04 10:31:25 -0300

    Removendo itens da venda

--- brewer/src/main/resources/static/javascripts/venda.tabela-itens.js
+++ brewer/src/main/resources/static/javascripts/venda.tabela-itens.js
@@ -24,6 +24,7 @@ Brewer.TabelaItens = (function() {
 		this.tabelaCervejasContainer.html(html);
 		$('.js-tabela-cerveja-quantidade-item').on('change', onQuantidadeItemAlterado.bind(this));
 		$('.js-tabela-item').on('dblclick', onDoubleClick);
+		$('.js-exclusao-item-btn').on('click', onExclusaoItemClick.bind(this));
 		
 	}
 	
@@ -44,6 +45,16 @@ Brewer.TabelaItens = (function() {
 	}
 	function onDoubleClick(evento) {
 		$(this).toggleClass('solicitando-exclusao');
+		
+	}
+	function onExclusaoItemClick(evento) {
+		var codigoCerveja = $(evento.target).data('codigo-cerveja');
+		var resposta = $.ajax({
+			url: 'item/' + codigoCerveja,
+			method: 'DELETE'
+		});
+		
+		resposta.done(onItemAtualizadoNoServidor.bind(this));
 	}
 	return TabelaItens;
 	
