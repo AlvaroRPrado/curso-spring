299d639c82823b2e696c324fd77b9ca64ea9440c
AlvaroRPrado <alvarorodrigues2010@gmail.com.br>
2019-10-07 08:58:00 -0300

    Simulando um escopo de view

--- brewer/src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
+++ brewer/src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
@@ -6,18 +6,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.SessionScope;
-
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.ItemVenda;
 
-@SessionScope
-@Component
-public class TabelaItensVenda {
+class TabelaItensVenda {
 
+	private String uuid;
 	private List<ItemVenda> itens = new ArrayList<>();
 	
+	public TabelaItensVenda(String uuid) {
+		this.uuid = uuid;
+	}
+
 	public BigDecimal getValorTotal() {
 		return itens.stream()
 				.map(ItemVenda::getValorTotal)
@@ -26,20 +26,19 @@ public class TabelaItensVenda {
 	}
 	
 	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
-			Optional<ItemVenda> itemVendaOptional = buscarItemPorCerveja(cerveja);
-		
+		Optional<ItemVenda> itemVendaOptional = buscarItemPorCerveja(cerveja);
 		
 		ItemVenda itemVenda = null;
-			if (itemVendaOptional.isPresent()) {
-				itemVenda = itemVendaOptional.get();
-				itemVenda.setQuantidade(itemVenda.getQuantidade() + quantidade);
-			} else {
-				itemVenda = new ItemVenda();
-				itemVenda.setCerveja(cerveja);
-				itemVenda.setQuantidade(quantidade);
-				itemVenda.setValorUnitario(cerveja.getValor());
-				itens.add(0, itemVenda);
-			}
+		if (itemVendaOptional.isPresent()) {
+			itemVenda = itemVendaOptional.get();
+			itemVenda.setQuantidade(itemVenda.getQuantidade() + quantidade);
+		} else {
+			itemVenda = new ItemVenda();
+			itemVenda.setCerveja(cerveja);
+			itemVenda.setQuantidade(quantidade);
+			itemVenda.setValorUnitario(cerveja.getValor());
+			itens.add(0, itemVenda);
+		}
 	}
 	
 	public void alterarQuantidadeItens(Cerveja cerveja, Integer quantidade) {
@@ -53,12 +52,12 @@ public class TabelaItensVenda {
 				.findAny().getAsInt();
 		itens.remove(indice);
 	}
-
+	
 	public int total() {
 		return itens.size();
 	}
 
-	public Object getItens() {
+	public List<ItemVenda> getItens() {
 		return itens;
 	}
 	
@@ -67,8 +66,34 @@ public class TabelaItensVenda {
 				.filter(i -> i.getCerveja().equals(cerveja))
 				.findAny();
 	}
-	
-	
 
+	public String getUuid() {
+		return uuid;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TabelaItensVenda other = (TabelaItensVenda) obj;
+		if (uuid == null) {
+			if (other.uuid != null)
+				return false;
+		} else if (!uuid.equals(other.uuid))
+			return false;
+		return true;
+	}
 	
 }
